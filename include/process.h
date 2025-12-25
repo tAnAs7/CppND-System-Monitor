cb8d642b13ddcd17676faf37caeb13525aa5c16b
Quang Tan <qtan.ng@gmail.com>
2025-12-26 00:49:40 +0700

    update

--- include/process.h
+++ include/process.h
@@ -5,13 +5,13 @@
 
 class Process {
  public:
-  Process(int pid);
-  int Pid();
-  std::string User();
-  std::string Command();
-  float CpuUtilization();
-  std::string Ram();
-  long int UpTime();
+  explicit Process(int pid);
+  int Pid() const;
+  std::string User() const;
+  std::string Command() const;
+  float CpuUtilization() const;
+  std::string Ram() const;
+  long int UpTime() const;
   bool operator<(Process const& a) const;
 
  private:
--- src/process.cpp
+++ src/process.cpp
@@ -2,36 +2,30 @@
 
 #include <unistd.h>
 
-#include <cctype>
-#include <sstream>
 #include <string>
-#include <vector>
 
 #include "linux_parser.h"
 
 using std::string;
-using std::to_string;
-using std::vector;
 
 Process::Process(int pid) : pid_(pid) {}
 
-int Process::Pid() { return pid_; }
+int Process::Pid() const { return pid_; }
 
-float Process::CpuUtilization() {
-  long uptime = LinuxParser::UpTime();
+float Process::CpuUtilization() const {
   long proc_uptime = LinuxParser::UpTime(pid_);
   long total_time = LinuxParser::ActiveJiffies(pid_);
   if (proc_uptime <= 0) return 0.0f;
   return static_cast<float>(total_time) / sysconf(_SC_CLK_TCK) / proc_uptime;
 }
 
-string Process::Command() { return LinuxParser::Command(pid_); }
+string Process::Command() const { return LinuxParser::Command(pid_); }
 
-string Process::Ram() { return LinuxParser::Ram(pid_); }
+string Process::Ram() const { return LinuxParser::Ram(pid_); }
 
-string Process::User() { return LinuxParser::User(pid_); }
+string Process::User() const { return LinuxParser::User(pid_); }
 
-long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
+long int Process::UpTime() const { return LinuxParser::UpTime(pid_); }
 
 bool Process::operator<(Process const& a) const {
   return CpuUtilization() > a.CpuUtilization();
